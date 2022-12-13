728d4afdcb112f620cacf420e29a4f3b72dd1d69
Galina Yesko <gyesko@fmv.local>
2022-12-13 10:29:18 +0300

    paymet alert demo

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function ode() {
+	alert("Ödenmiştir");
+}
+
+function gizle() {
+
+	
+	//document.getElementById('gizli1').style.display='none';
+	//$('#gizli1').addClass('hidden');
+	$('#gizli1').hide();
+}
